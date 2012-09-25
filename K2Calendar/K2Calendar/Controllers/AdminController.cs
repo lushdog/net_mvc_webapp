647a3d9634d703bef6ff4885c4106e92f54cb9ab
lushdog <lushdog@771c05b8-c0c6-427d-8cd5-0fb121234a84>
2012-09-25 21:44:51 +0000

    First checking of Admin/Users.cshtml.
    git-svn-id: https://lulz.unfuddle.com/svn/lulz_k2cal@26 771c05b8-c0c6-427d-8cd5-0fb121234a84

--- K2Calendar/K2Calendar/Controllers/AdminController.cs
+++ K2Calendar/K2Calendar/Controllers/AdminController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using K2Calendar.Models;
 using System.Data.SqlClient;
+using System.Web.Mvc;
 using System.Web.Security;
+using K2Calendar.Models;
+using System.Collections.Generic;
 
 namespace K2Calendar.Controllers
 {
@@ -26,18 +24,21 @@ namespace K2Calendar.Controllers
         //TODO: sort by parameter
         public ActionResult Users(int pageNumber = 1)
         {
-            UserInfoSummaryModel userSummary;
+            List<UserInfoSummaryModel> userSummaries = new List<UserInfoSummaryModel>();
             using (SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.
                                 ConnectionStrings["ApplicationServices"].ConnectionString))
             {
                 conn.Open();
+                //TODO: if we get over a million rows we should convert to :
+                //http://www.4guysfromrolla.com/webtech/042606-1.shtml
                 using (SqlCommand command = new SqlCommand(@";WITH Results_CTE AS
                                                                 (
                                                                     SELECT
                                                                         aspnet_Users.UserName, aspnet_Users.UserId, 
                                                                         aspnet_Membership.LoweredEmail, aspnet_Membership.LastLoginDate,
                                                                         UserInfoModels.FirstName, UserInfoModels.LastName, UserInfoModels.Id,
-                                                                        ROW_NUMBER() OVER (ORDER BY aspnet_Users.UserName) AS RowNum
+                                                                        ROW_NUMBER() OVER (ORDER BY aspnet_Users.UserName) AS RowNum,
+                                                                        Count(*) OVER() As TotalRows 
                                                                     FROM aspnet_Users INNER JOIN aspnet_Membership ON aspnet_Users.UserId = aspnet_Membership.UserId 
                                                                     INNER JOIN UserInfoModels ON aspnet_Users.UserId = UserInfoModels.MembershipId 
                                                                 )
@@ -51,20 +52,30 @@ namespace K2Calendar.Controllers
 
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        reader.Read();
-                        userSummary = new UserInfoSummaryModel { FirstName = reader["FirstName"].ToString(), 
-                                                                LastName = reader["LastName"].ToString(), 
-                                                                Username = reader["UserName"].ToString(), 
-                                                                Email = reader["LoweredEmail"].ToString(),
-                                                                MembershipId = reader["UserId"].ToString(),
-                                                                UserId = Convert.ToInt32(reader["Id"]),
-                                                                LastLogin = DateTime.Parse(reader["LastLoginDate"].ToString())
-                                                                };
-                        userSummary.Roles = Roles.GetRolesForUser(userSummary.Username);
+                        while (reader.Read())
+                        {
+                            var userSummary = new UserInfoSummaryModel
+                            {
+                                FirstName = reader["FirstName"].ToString(),
+                                LastName = reader["LastName"].ToString(),
+                                Username = reader["UserName"].ToString(),
+                                Email = reader["LoweredEmail"].ToString(),
+                                MembershipId = reader["UserId"].ToString(),
+                                UserId = Convert.ToInt32(reader["Id"]),
+                                LastLogin = DateTime.Parse(reader["LastLoginDate"].ToString())                                
+                            };
+                            //TODO: could optimize this block
+                            userSummary.Roles = Roles.GetRolesForUser(userSummary.Username);
+                            userSummaries.Add(userSummary);
+                            ViewBag.TotalRows = Convert.ToInt32(reader["TotalRows"]);
+                        }                    
                     }
                 }
-            }            
-            return View();
+            }
+            ViewBag.PageSize = PAGE_SIZE;
+            ViewBag.PageNum = pageNumber;
+            ViewBag.TotalPages = Math.Ceiling((double)ViewBag.TotalRows / PAGE_SIZE);
+            return View(userSummaries);
         }
     }
 }
