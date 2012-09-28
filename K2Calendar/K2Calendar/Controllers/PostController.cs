3d44aeedeea350107cb55321279c1415a737bbcf
lushdog <lushdog@771c05b8-c0c6-427d-8cd5-0fb121234a84>
2012-09-28 02:59:35 +0000

    Post description in Post/Index.cshtml is truncated. Post in Post/Index.cshtml can link to Post/Details/id.
    git-svn-id: https://lulz.unfuddle.com/svn/lulz_k2cal@32 771c05b8-c0c6-427d-8cd5-0fb121234a84

--- K2Calendar/K2Calendar/Controllers/PostController.cs
+++ K2Calendar/K2Calendar/Controllers/PostController.cs
@@ -14,7 +14,7 @@ namespace K2Calendar.Controllers
     {
         private AppDbContext dbContext = new AppDbContext();
         private const int PAGE_SIZE = 15;
-
+        private const int DESCRIPTION_SUMMARY_LENGTH = 400;
         //TODO: index, show recent posts in a summary type format (1)
         // GET: /Post/
         public ViewResult Index(int pageNumber = 1)
@@ -22,6 +22,14 @@ namespace K2Calendar.Controllers
             ViewBag.NumPages = Math.Ceiling((double)dbContext.Posts.Count() / PAGE_SIZE);
             ViewBag.PageNum = pageNumber;
             var posts = dbContext.Posts.Include(p => p.Rank).Include(p => p.PostedBy).OrderBy(p => p.Id).Skip((pageNumber - 1) * PAGE_SIZE).Take(PAGE_SIZE);
+            foreach (PostModel post in posts)
+            {
+                if (post.Description.Length > DESCRIPTION_SUMMARY_LENGTH)
+                {
+                    var d = post.Description.Remove(DESCRIPTION_SUMMARY_LENGTH - 3);
+                    post.Description  = d + "...";
+                }
+            }
             return View(posts.ToList());
         }
 
